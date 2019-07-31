0a4e6a85c3e515c05afa429a91d50c7e48b71532
SoulfulArt <paulofariamelo@gmail.com>
2019-07-31 07:24:52 -0300

    Graph reload working.

--- main.py
+++ main.py
@@ -10,9 +10,13 @@ from kivy.uix.popup import Popup
 from kivy.config import Config
 from kivy.garden.graph import Graph, MeshLinePlot
 from math import sin
+from rtlsdr import *
+from kivy.clock import Clock
+import matplotlib.font_manager as fm
+from kivy.properties import ObjectProperty
+from kivy.uix.image import Image
 
-Config.set('kivy','window_icon','path/to/icon.ico')
-
+#Classes definition
 class SA251760Gui(FloatLayout):
 	pass
 
@@ -29,15 +33,48 @@ class LineRectangle(Widget):
     pass
 
 class SA251760App(App):
-	globalMinFreq = 25
-	globalMaxFreq = 1760
-	plot = plt
-	plot.plot([1, 23, 2, 4])
-	plot.ylabel('some numbers')
-	plt.gcf()
-	plt.savefig('/home/paulo/25to1760MHzSA/images/graph.png')
+	globalMinFreq = 798
+	globalMaxFreq = 801
+	fontName = '/home/paulo/25to1760MHzSA/fontComputer/cmunsx.ttf'
+	imageSource = '/home/paulo/25to1760MHzSA/images/graph.png'
+	
 	def build(self):
+
+		Clock.schedule_interval(self.update, 0.002)
+		
 		return SA251760Gui()
+
+	def update(self, dt):
+
+		sdr = RtlSdr()
+		
+		# configure device
+		sdr.center_freq = ((self.globalMinFreq + self.globalMaxFreq)/2)*1e6
+		sdr.gain = 4
+
+		samples = sdr.read_samples(256*1024)
+		sdr.close()
+	
+		fig = plt.figure()
+		fig.patch.set_alpha(0)
+	
+		prop = fm.FontProperties(fname = self.fontName, size = 14)
+
+		#Axis inputs
+		ax = plt.psd(samples, NFFT=1024, Fc=sdr.center_freq/1e6, color='k')
+		ax = plt.xlim(self.globalMinFreq, self.globalMaxFreq)
+
+		#Axis layout 
+		ax = plt.xlabel('Frequency [MHz]', fontproperties=prop, color='w')
+		ax = plt.ylabel('Relative power [dB]', fontproperties=prop, color='w')
+		ax = fig.add_subplot(111)
+		ax.grid(True, linestyle='-.',color='w')
+		ax.tick_params(labelcolor='w', labelsize='medium', width=3)
+		ax.patch.set_alpha(0)
+		
+		self.root.ids.graph_kivy.reload()
+		
+		plt.savefig(self.imageSource)
 
 if __name__ == '__main__':
 	SA251760App().run()
